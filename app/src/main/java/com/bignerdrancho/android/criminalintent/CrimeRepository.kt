adee4951f21c3c9513782a5f9c306855b8a8bd73
Pushka-Gonka <mzabrodin94@gmail.com>
2021-10-09 20:22:11 +0700

    капт для рум не завелся

--- app/src/main/java/com/bignerdrancho/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdrancho/android/criminalintent/CrimeRepository.kt
@@ -1,6 +1,7 @@
 package com.bignerdrancho.android.criminalintent
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.room.Room
 import database.CrimeDatabase
 import java.util.*
@@ -16,8 +17,8 @@ class CrimeRepository private constructor(context: Context){
     ).build()
     private val crimeDao = database.crimeDao()
 
-    fun getCrimes(): List<Crime> = crimeDao.getCrimes()
-    fun getCrime(id: UUID): Crime? = crimeDao.getCrime(id)
+    fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
+    fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
 
 
     companion object {
